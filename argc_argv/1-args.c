9eed1f2bc2012b2fe8109a08cae7497bd7dde95f
Rami <ramirima@hotmail.com>
2025-10-22 15:16:15 +0200

    adding task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Programme qui affiche le numero d'arguments
+ * @argc: Nombre d'arguments passés au programme
+ * @argv: Tableau contenant les arguments
+ *
+ * Return: 0 en cas de succès
+ */
+int main(int argc, char *argv[])
+{
+    (void)argv;
+	printf("%d\n", argc);
+	return (0);
+}

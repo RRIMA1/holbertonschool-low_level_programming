674cf3f7af7389a5436f5facb9d49b3d41f0c09d
Rami <ramirima@hotmail.com>
2025-10-23 17:11:16 +0200

    fixing task0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	}
 
 	ar = malloc(size * sizeof(char));
-	if (arr == NULL)
+	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		ar[i] = c;

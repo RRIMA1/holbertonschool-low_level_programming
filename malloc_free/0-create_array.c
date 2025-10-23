8d92f92cc6d1747fe37a00e1530592e7a77b4100
Rami <ramirima@hotmail.com>
2025-10-23 16:57:25 +0200

    adding task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <main.h>
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *ar;
+
+	ar = malloc(size * sizeof(char));
+	for (int i = 0; i < size; i++)
+		ar[i] = c;
+
+	return (ar);
+}
+
+
+int main(void)
+{
+
+}

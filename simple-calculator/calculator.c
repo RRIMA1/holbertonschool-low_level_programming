67a3d5de8ae7935ef8977af757026f20b1a0d357
Rami <ramirima@hotmail.com>
2026-02-27 10:02:04 +0100

    testing the checker

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -3,18 +3,29 @@
 
 void display_menu(void){
     int choice;
-    printf("Simple Calculator \n 1) Add\n 2) Subtract\n 3) Multiply\n 4) Divide\n 0) Quit\n Choice: ");
-    scanf("%d", &choice);
+    int break_loop=0;
+    printf("Simple Calculator \n 1) Add\n 2) Subtract\n 3) Multiply\n 4) Divide\n 0) Quit\n ");
+    while (break_loop==0)
+    {
+       printf("Choice:");
+       scanf("%d", &choice);
+    if (choice<0 || choice>4){
+       printf("Invalid choice\n");
+    }
     if (choice==0){
         printf("\n Bye!");
-        return ;
+        break_loop=1 ;
     }
+
+    }
+    
+
 
 }
 
 int main(void)
 {
 
-  display_menu();
+  //display_menu();
   return 0;  
 }

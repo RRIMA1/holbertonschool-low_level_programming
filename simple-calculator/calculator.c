5d51a5f0c1b36b975af9857813dd4c1c4fa01fe2
Rami <ramirima@hotmail.com>
2026-02-27 10:45:26 +0100

    adding + / -

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,8 +2,10 @@
 #include "calculator.h"
 
 void display_menu(void){
+
     int choice;
     int break_loop=0;
+    int a,b,res;
     printf("Simple Calculator \n 1) Add\n 2) Subtract\n 3) Multiply\n 4) Divide\n 0) Quit\n ");
     while (break_loop==0)
     {
@@ -11,20 +13,60 @@ void display_menu(void){
        scanf("%d", &choice);
     if (choice<0 || choice>4){
        printf("Invalid choice\n");
+       continue;
     }
     if (choice==0){
         printf("\n Bye!");
         break_loop=1 ;
     }
-
+    switch (choice)
+    {
+    case 1:
+        /* code */
+        printf("A: ");
+        scanf("%d",&a);
+        printf("B: ");
+        scanf("%d", &b);
+        res=a+b;
+        break_loop=1;
+        break;
+    case 2:
+        /* code */
+        printf("A: ");
+        scanf("%d",&a);
+        printf("B: ");
+        scanf("%d", &b);
+        res=a-b;
+        break_loop=1;
+        break;
+    case 3:
+        /* code */
+        printf("A: ");
+        scanf("%d",&a);
+        printf("B: ");
+        scanf("%d", &b);
+        res=a*b;
+        break_loop=1;
+        break; 
+    case 4:
+        /* code */
+        printf("A: ");
+        scanf("%d",&a);
+        printf("B: ");
+        scanf("%d", &b);
+        printf("%.2f", (float)a/b);
+        return;
+        break;    
+    default:
+        break;
     }
-    
 
-
+}
+    printf("Result: %d", res);
 }
 
 int main(void)
 {
-
+  display_menu();
   return 0;  
 }
